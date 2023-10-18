47cbdedd0afa584e533a6d9e10abe27926c1e8c5
papparazzo <pappi-@gmx.de>
2023-10-18 06:52:21 +0200

    sql-statements added

---
+++ sql/sfw2_menu.sql
@@ -0,0 +1,33 @@
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2018  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS `{TABLE_PREFIX}_menu` (
+    `Id`       int         UNSIGNED auto_increment primary key,
+    `ParentId` int(11)     UNSIGNED NOT NULL,
+    `Position` tinyint(11) UNSIGNED NOT NULL,
+    `PathId`   int(11)     UNSIGNED NOT NULL,
+    `Name`     varchar(25)          NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `{TABLE_PREFIX}_menu` ADD PRIMARY KEY (`Id`);
+/* FIXME Do not forget to add foreign key constrains! */
